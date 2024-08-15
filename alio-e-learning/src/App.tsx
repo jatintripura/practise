bba3a012606b84c1ebd69fcb0283cbfc3e7385d9
jatintripura <jatintripura49@gmail.com>
2024-08-15 21:18:25 +0600

    navbar complete

---
+++ alio-e-learning/src/App.tsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <>
+      <main className="overflow-x-hidden text-dark bg-white">
+        <Navbar />
+      </main>
+    </>
+  );
+};
+export default App;
